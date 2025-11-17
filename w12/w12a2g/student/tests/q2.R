2edcb53e3bbba1dc4698b08d24b5af867cf66345
David Broockman <dbroockman@users.noreply.github.com>
2025-11-16 19:32:20 -0800

    fix q2

--- w12/w12a2g/student/tests/q2.R
+++ w12/w12a2g/student/tests/q2.R
@@ -1,18 +1,5 @@
 test = list(
   name = "q2",
   cases = list(
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 0.5,
-      code = {
-        . = "# BEGIN PROMPT
-        q2.answer <- '...'
-        " # END PROMPT
-        # BEGIN SOLUTION NO PROMPT
-        q2.answer <- 'e'
-        # END SOLUTION
-      }
-    )
   )
 )
